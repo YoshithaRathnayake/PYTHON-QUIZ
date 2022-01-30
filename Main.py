494f3610faf8516951562f2369fa3c112cce5256
YOSHITHA RATHNAYAKE <Y.S.B.Rathnayake2008@gmail.com>
2022-01-30 14:49:51 +0530

    Create Main.py

---
+++ Main.py
@@ -0,0 +1,55 @@
+#User Name
+
+UserName = input("Hi, I am Yoshitha Rathnayake. What is your name? - ")
+print("Hello, " + UserName + ". Welcome to my Quiz that I made using Python (Language). I am Asking 5 question from you.")
+
+#Question 1
+
+Question1 = input("1. What is the highest mountain in the world? = ")
+Answer1 = "Mount Everest"
+if Question1 == Answer1:
+    print("Well done! You are correct. Next >")
+else:
+    print("You are wrong! Answer : Mount Everest")
+
+#Question 2
+
+Question2 = input("2. What is the longest river in the world? = ")
+Answer2 = "Nile River"
+if Question2 == Answer2:
+    print("Great! You are correct. Next >")
+else:
+    print("You are wrong! Answer : Nile River")
+
+#Question 3
+
+Question3 = input("3. What is the deepest point in the world? = ")
+Answer3 = "Mariana Trench"
+if Question3 == Answer3:
+    print("Well done! You are correct. Next >")
+else:
+    print("You are wrong! Answer : Mariana Trench")
+
+#Question 4
+
+Question4 = input("4. What is the largest continent in the world? = ")
+Answer4 = "Asia"
+if Question4 == Answer4:
+    print("Great! You are correct. Next >")
+else:
+    print("You are wrong! Answer : Asia")
+
+#Question 5
+
+Question5 = input("5. What is the biggest desert in the world? = ")
+Answer5 = "Sahara Desert"
+if Question5 == Answer5:
+    print("Well done! You are correct. Next >")
+else:
+    print("You are wrong! Answer : Sahara Desert")
+
+#"Thank You" Greating
+
+print("Thank you for answer my Quiz!")
+
+
